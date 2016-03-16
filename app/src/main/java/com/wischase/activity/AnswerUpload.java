d4d32f0ab997b5db4351a21b1fdde6eefc9b2245
disha <gayathriraju84@gmail.com>
2016-03-15 22:27:20 -0500

    Adding ExplanationUpload activity

--- app/src/main/java/com/wischase/activity/AnswerUpload.java
+++ app/src/main/java/com/wischase/activity/AnswerUpload.java
@@ -9,6 +9,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.wischase.Category;
 import com.wischase.Question;
@@ -19,29 +22,22 @@ import com.wischase.view.menu.ScrollingActivity;
 import java.util.List;
 
 public class AnswerUpload extends ScrollingActivity {
-
-    List<Question> questionList;
-    int currentCorrect = 0;
-    int totalCurrent = 0;
-    int questionIndex = 0;
-
+Question question;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer_upload);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-       Intent inputIntent = getIntent();
-       Question question =  inputIntent.getParcelableExtra("question");
-        String s=question.getQuestionText();
-        Log.i(s, "just display");
- /*       Question userInput = (Question)(inputIntent.writeToParcel(parcel));
+        Intent inputIntent = getIntent();
+        question = inputIntent.getParcelableExtra("question");
+        /*       Question userInput = (Question)(inputIntent.writeToParcel(parcel));
         int gradeInput = inputIntent.getIntExtra(ActivityConstants.GRADE_INPUT,0);
         DBHandler dbHandle = new DBHandler(this);
         int categoryIdInput = userInput.getSubCategory().get(0).getCategoryId();
         questionList = dbHandle.getAllQuestions(categoryIdInput, gradeInput);
 */
-        setContentView(R.layout.activity_answer_upload);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -52,10 +48,59 @@ public class AnswerUpload extends ScrollingActivity {
             }
         });
     }
+
+    public int validatingAnswer(View view) {
+        EditText op1 = (EditText) findViewById(R.id.up_option1);
+        String option1 = op1.getText().toString();
+        EditText op2 = (EditText) findViewById(R.id.up_option2);
+        String option2 = op2.getText().toString();
+        EditText op3 = (EditText) findViewById(R.id.up_option3);
+        String option3 = op3.getText().toString();
+        EditText op4 = (EditText) findViewById(R.id.up_option4);
+        String option4 = op4.getText().toString();
+        int i=-1;
+           if ((option1.equals(" ")) || (option2.equals(" "))) {
+                Toast.makeText(getBaseContext(), "Need minimum 2 options to upload", Toast.LENGTH_LONG).show();
+            } else {
+                RadioGroup radioGroup = (RadioGroup) findViewById(R.id.options);
+                int index = radioGroup.indexOfChild(findViewById(radioGroup.getCheckedRadioButtonId()));
+                if (index == -1) {
+                    Toast.makeText(getBaseContext(), "Please select correct answer", Toast.LENGTH_LONG).show();
+
+                } else {
+                    i = 0;
+                    question.setOptionOne(option1);
+                    question.setOptionTwo(option2);
+                    question.setOptionThree(option3);
+                    question.setOptionFour(option4);
+                    question.setCorrectAnswer(index + 1);
+                    /* Need to set category id, userid,grade*/
+
+                }
+            }
+        return i;
+    }
     public void uploadAnswer(View view)
     {
+        if((validatingAnswer(view))==0)
+        {
+            Parcel questionParcel=null;
+            question.writeToParcel(questionParcel,0);
+            DBHandler dbHandler=new DBHandler(this);
+           long i= dbHandler.insertQuestion(question);
+            Toast.makeText(getBaseContext(),"successful",Toast.LENGTH_LONG).show();
+        }
+    }
+    public void nextExplanation(View view)
+    {
+        if((validatingAnswer(view))==0)
+        {
+            setContentView(R.layout.activity_explanation_upload);
+            Intent intent=new Intent(this,ExplanationUpload.class);
+            intent.putExtra("answer",question );
+            startActivity(intent);
 
+        }
 
     }
-
 }
