064060c7c8905bc07ba14794b1b56b9d5683d020
sri_suki <sukiwillbe1981@gmail.com>
2016-04-03 19:41:47 -0500

    Parcelable Changes to Login Screen pushed to Github

--- app/src/main/java/com/wischase/activity/LogInScreen.java
+++ app/src/main/java/com/wischase/activity/LogInScreen.java
@@ -3,11 +3,11 @@ package com.wischase.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.wischase.R;
+import com.wischase.UserInfo;
 import com.wischase.db.DBHandler;
 import com.wischase.view.menu.ScrollingActivity;
 
@@ -19,31 +19,30 @@ public class LogInScreen extends ScrollingActivity {
         setContentView(R.layout.activity_log_in_screen);
     }
 
-            public void signUp(View v) {
-                EditText ed1 = (EditText)findViewById(R.id.userNameEntry);
-                String UsrName = ed1.getText().toString();
-                DBHandler DB1 = new DBHandler(getApplicationContext());
-                long UserId = DB1.insertUserInfo(UsrName);
-                long x = -1;
-                if (UserId != x) {
-                    //Toast.makeText(getApplicationContext(), "UserId:" +UserId, Toast.LENGTH_SHORT).show();
-                    setContentView(R.layout.activity_selectionscreen);
-                    Intent i = new Intent(getBaseContext(),Selectionscreen.class);
-                    startActivity(i);
-                }
-                    else{
-                   //Logic is yet to be decided
-
-                }
-
-            }
+    // On clicking Sign In this method is called
     public void signIn(View v) {
-        //THis is incomplete
+
+        //Retrieve the user name from the User
         EditText ed1 = (EditText)findViewById(R.id.userNameEntry);
         String UsrName = ed1.getText().toString();
+
+        //Insert or retrieve the user details from DB
         DBHandler DB1 = new DBHandler(getApplicationContext());
-       // long UserId = DB1.getUserInfo(UsrName);
-        }
+        long UserId = DB1.insertUserInfo(UsrName);
+
+        Toast.makeText(getBaseContext(),"Value is " +UserId, Toast.LENGTH_LONG).show();
+        Toast.makeText(getBaseContext(),UsrName, Toast.LENGTH_LONG).show();
+        // Passing the User Info to the next Screen
+        setContentView(R.layout.activity_selectionscreen);
+        UserInfo Uinfo = new UserInfo();
+        Uinfo.getuserInfo(UserId, UsrName);
+        Intent i = new Intent(getBaseContext(),Selectionscreen.class);
+        i.putExtra("UserId",Uinfo);
+        i.putExtra("UsrName",Uinfo);
+
+
+        //Display the Screen
+        startActivity(i);
     }
 
-
+}
