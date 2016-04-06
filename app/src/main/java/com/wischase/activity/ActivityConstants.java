7efe5982b2f04ce945658deabc6d44fa0303390c
spsush <spsush@gmail.com>
2016-04-05 21:30:01 -0500

    spinners populate; changed to onclick.

--- app/src/main/java/com/wischase/activity/ActivityConstants.java
+++ app/src/main/java/com/wischase/activity/ActivityConstants.java
@@ -7,7 +7,7 @@ public class ActivityConstants {
     public static final String USER_INPUT = "userInput";
     public static final String GRADE_INPUT = "gradeInput";
     public static final String QUESTIONS = "questions";
-    
+
     /* private String CATEGORY,SUBCATEGORY;
     private long USERID;
     public long getUSERID(){return USERID;}
