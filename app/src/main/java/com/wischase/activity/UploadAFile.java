80fd2bac8ba924beb5a630050263c806266299c8
disha <gayathriraju84@gmail.com>
2016-04-20 13:30:49 -0500

    Uploading a excel file using JXL jar

--- app/src/main/java/com/wischase/activity/UploadAFile.java
+++ app/src/main/java/com/wischase/activity/UploadAFile.java
@@ -1,38 +1,42 @@
 package com.wischase.activity;
-
-import android.app.SearchManager;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.SearchView;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import com.wischase.Category;
+import com.wischase.Question;
 import com.wischase.R;
-import com.wischase.view.menu.ScrollingActivity;
-
+import com.wischase.db.DBHandler;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URI;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
 
 public class UploadAFile extends UpdateTable {
     Category userInput;
     int grade;
     int categoryIdInput;
     private File dir;
-    boolean mExternalStorageAvailable = false;
-    boolean mExternalStorageWriteable = false;
+    Question upload = new Question();
+    private static final int REQUEST_PICK_FILE = 1;
+
+    private TextView mFilePathTextView;
+    private Button mStartActivityButton;
+    private File selectedFile;
+
+    String s = "";
+    File sdcard1 = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+    File internal = Environment.getDownloadCacheDirectory().getAbsoluteFile();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,48 +44,181 @@ public class UploadAFile extends UpdateTable {
         setContentView(R.layout.activity_upload_afile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Intent intent=getIntent();
-        userInput=(Category)(intent.getParcelableExtra(ActivityConstants.USER_INPUT));
-        grade=intent.getIntExtra(ActivityConstants.GRADE_INPUT,0);
-        super.updateCategoryTable(userInput, grade);
-        categoryIdInput= userInput.getSubCategory().get(0).getCategoryId();
+        Intent intent = getIntent();
+        if (getIntent().hasExtra(ActivityConstants.USER_INPUT)) {
+            userInput = (Category) (intent.getParcelableExtra(ActivityConstants.USER_INPUT));
+            grade = intent.getIntExtra(ActivityConstants.GRADE_INPUT, 0);
+            super.updateCategoryTable(userInput, grade);
+            categoryIdInput = userInput.getSubCategory().get(0).getCategoryId();
+        }
+        // mFilePathTextView = (TextView)findViewById(R.id.filena);
+        //   mFilePathTextView.setText(s);
+        /*if (getIntent().hasExtra(FilePickerActivity.EXTRA_FILE_PATH)) {
+            s = getIntent().getStringExtra(FilePickerActivity.EXTRA_FILE_PATH);
 
-         /*  SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView=null;
-        //= (SearchView) menu.findItem(R.id.menu_search).getActionView();
-        // Assumes current activity is the searchable activity
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
-        //return true;
-        dir = getFilesDir();
-        dir.listFiles();
-        */
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            checkAvailablity();
+        }*/    }
+
+    public void checkAvailablity(View view) {
+        try {
+            // s is the absolute file path
+            FileInputStream fIn = new FileInputStream("/storage/emulated/0/Download/upload.xls");
+             Workbook wb = Workbook.getWorkbook(fIn);
+            Sheet s = wb.getSheet(0);
+            int row = s.getRows();
+            int col = s.getColumns();
+            int failures=0;
+            String rowNotUploaded="Rows that are not uploaded: ";
+            String xx = " ";
+            for (int i = 1; i < row; i++) {
+                Cell z = s.getCell(0,i);
+                xx = z.getContents();
+                //checking Question is given
+               quest:
+                if (!xx.equals("")) {
+//                Toast.makeText(getApplicationContext(), xx, Toast.LENGTH_LONG).show();
+                    upload.setQuestionText(xx);
+                    z = s.getCell(1, i);
+                    xx = z.getContents();
+                    //Checking option1 is given
+                           if (!xx.equals("")) {
+                        upload.setOptionOne(xx);
+                        z = s.getCell(2,i);
+                        xx = z.getContents();
+                        //Checking option2 is given
+                        if (!xx.equals("")) {
+                            upload.setOptionTwo(xx);
+                            z = s.getCell(3,i);
+                            xx = z.getContents();
+                            upload.setOptionThree(xx);
+                            z = s.getCell(4,i);
+                            xx = z.getContents();
+                            upload.setOptionFour(xx);
+                            z = s.getCell(5,i);
+                            xx = z.getContents();
+                            //Checking answer is given
+                            if (!xx.equals("")) {
+                                int answer = Integer.parseInt(xx);
+                                upload.setCorrectAnswer(answer);
+                                z = s.getCell(6,i);
+                                xx = z.getContents();
+                                upload.setExplanation(xx);
+                                upload.setCategoryId(categoryIdInput);
+                                upload.setUserid(1);
+                                upload.setGrade(grade);
+                                DBHandler dbHandler = new DBHandler(this);
+                                long questNo = dbHandler.insertQuestion(upload);
+                                dbHandler.close();
+                            }
+                            //Row numbers are saved and displayed at last.
+                            else
+                            {
+                                rowNotUploaded= rowNotUploaded+i+" ";
+                                failures=1;
+                                break quest;
+                            }
+
+                        }//Row numbers are saved and displayed at last.
+                        else
+                        {
+                            rowNotUploaded= rowNotUploaded+i+" ";
+                            failures=1;
+                            break quest;
+                        }
+
+                    }//Row numbers are saved and displayed at last.
+                    else
+                    {
+                        rowNotUploaded= rowNotUploaded+i+" ";
+                        failures=1;
+                        break quest;
+                    }
+
+                }
+                //Row numbers are saved and displayed at last.
+                else
+                {
+                    rowNotUploaded= rowNotUploaded+i+" ";
+                    failures=1;
+
+                }
             }
-        });
-    }
+            Toast.makeText(getBaseContext(), "Completed Uploading", Toast.LENGTH_LONG).show();
 
-    public void onSearchClick(View view) {
-        String state = Environment.getExternalStorageState();
-        EditText editText = (EditText) findViewById(R.id.up_file);
-        String file_name = editText.getText().toString();
-
-        if (Environment.DIRECTORY_DOWNLOADS.equals(state)) {
-            mExternalStorageAvailable = true;
-            File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-            File file = new File(path, file_name);
-            try {
-                path.mkdirs();
-                InputStream inputStream = new FileInputStream(file);
-                Toast.makeText(getBaseContext(),file_name,Toast.LENGTH_LONG).show();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+            if(failures==1){
+                Toast.makeText(getBaseContext(),rowNotUploaded,Toast.LENGTH_LONG).show();
             }
+        }catch (FileNotFoundException e1) {
+            // e1.printStackTrace();
+            Toast.makeText(getApplicationContext(), "file not found exception", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "io exception", Toast.LENGTH_LONG).show();
+        } catch (BiffException e) {
+            // e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "biff exception", Toast.LENGTH_LONG).show();
         }
     }
+
+
+ /*   public void browseFolderSD(View v) {
+        if(Environment.isExternalStorageRemovable()) {
+            // Create a new Intent for the file picker activity
+            Intent intent = new Intent(this, FilePickerActivity.class);
+            // Only make .xls files visible
+            ArrayList<String> extensions = new ArrayList<String>();
+            extensions.add(".xls");
+            intent.putExtra(FilePickerActivity.EXTRA_ACCEPTED_FILE_EXTENSIONS, extensions);
+            //intent.putExtra(FilePickerActivity.EXTRA_FILE_PATH, Environment.getExternalStorageDirectory());
+           // intent.putExtra(FilePickerActivity.EXTRA_FILE_PATH,"/sdcard/");
+            // Start the activity
+            // startActivityForResult(intent, REQUEST_PICK_FILE);
+            //  break;
+            startActivity(intent);
+        }
+        else
+            Toast.makeText(getApplicationContext(),"SD card is not available",Toast.LENGTH_LONG).show();
+
+    }
+    public void browseFolderPhone(View view)
+    {
+        Intent intent = new Intent(this, FilePickerActivity.class);
+        ArrayList<String> extensions = new ArrayList<String>();
+        extensions.add(".xls");
+        intent.putExtra(FilePickerActivity.EXTRA_ACCEPTED_FILE_EXTENSIONS, extensions);
+        //these version needs checking for download directory alone
+        if((android.os.Build.VERSION.RELEASE.equals("4.3"))||(android.os.Build.VERSION.RELEASE.startsWith("4.2"))||(android.os.Build.VERSION.RELEASE.startsWith("4.1"))||(android.os.Build.VERSION.RELEASE.startsWith("4.0")))
+        {
+            if(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).isDirectory())
+            {
+                intent.putExtra(FilePickerActivity.EXTRA_FILE_PATH,  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS));}
+        }
+        else
+        {
+            intent.putExtra(FilePickerActivity.EXTRA_FILE_PATH,Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS));
+        }startActivity(intent);
+
+    }
+    public void browseOneDrive(View view)
+    {
+
+    }*/
 }
+   // @Override
+ /*  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK) {
+            switch(requestCode) {
+                case REQUEST_PICK_FILE:
+                    if(data.hasExtra(FilePickerActivity.EXTRA_FILE_PATH)) {
+                        s=data.getStringExtra(FilePickerActivity.EXTRA_FILE_PATH);
+                        // Get the file path
+                       // selectedFile = new File(data.getStringExtra(FilePickerActivity.EXTRA_FILE_PATH));
+                        // Set the file path text view
+                        mFilePathTextView.setText(s);
+mFilePathTextView.setVisibility(View.VISIBLE);
+                        //Now you have your selected file, You can do your additional requirement with file.
+                    }
+                    break;
+            }
+        }
+    }*/
+
