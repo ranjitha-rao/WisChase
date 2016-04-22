c21d0d48ec992432365c3caa3b405eb4cc637189
Ranjitha-rao <ranjitha.m.rao@gmail.com>
2016-04-22 15:13:48 -0500

    Added snackbar to display explanation. The quiz is now in 3 questions batch. This has to be updated to 20 questions.

--- app/src/main/java/com/wischase/activity/TakeAQuiz.java
+++ app/src/main/java/com/wischase/activity/TakeAQuiz.java
@@ -1,45 +1,63 @@
 package com.wischase.activity;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.CompoundButtonCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Chronometer;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.wischase.Category;
 import com.wischase.Question;
 import com.wischase.R;
 import com.wischase.db.DBHandler;
+import com.wischase.view.CustomButton;
+import com.wischase.view.CustomRadioButton;
 import com.wischase.view.CustomTextView;
 import com.wischase.view.menu.ScrollingActivity;
 
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 import java.util.List;
 
     public class TakeAQuiz extends ScrollingActivity {
 
         List<Question> questionList;
-        String currentCorrectAnswer;
+        int currentCorrectAnswer;
         int totalCorrect = 0;
         int questionIndex = 0;
+        int userEnteredAnswer = 0;
+        int[] answerOptionRadioButton = {R.id.optionOneRadio,R.id.optionTwoRadio,R.id.optionThreeRadio,R.id.optionFourRadio};
+        int offset = 0;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             Intent inputIntent = getIntent();
             Category userInput = (Category)(inputIntent.getParcelableExtra(ActivityConstants.USER_INPUT));
-            int gradeInput = inputIntent.getIntExtra(ActivityConstants.GRADE_INPUT,0);
+            int gradeInput = (int) inputIntent.getLongExtra(ActivityConstants.GRADE_INPUT,0) - 2;
             DBHandler dbHandle = new DBHandler(this);
             int categoryIdInput = userInput.getSubCategory().get(0).getCategoryId();
-            questionList = dbHandle.getAllQuestions(categoryIdInput, gradeInput);
+            offset = inputIntent.getIntExtra(ActivityConstants.QUIZAGAIN,0) * 3;
+            questionList = dbHandle.getAllQuestions(categoryIdInput, gradeInput, offset);
             setContentView(R.layout.activity_take_aquiz);
-            currentCorrectAnswer = String.valueOf(questionList.get(questionIndex).getCorrectAnswer());
             Chronometer timeElapsed = (Chronometer) findViewById(R.id.timeElapsed);
-            updateQuestion(questionList.get(questionIndex));
+            updateQuestion(Boolean.FALSE, null);
             updateScoreCard(userInput, gradeInput);
             timeElapsed.start();
         }
@@ -49,49 +67,106 @@ import java.util.List;
             categoryText.setText(userInput.getCategory());
             CustomTextView gradeText = (CustomTextView) findViewById(R.id.grade);
             gradeText.setText(String.valueOf(gradeInput));
+            updateTotalQuestionAnswered();
+            updateTotalCorrect();
+            CustomTextView subCategory = (CustomTextView) findViewById(R.id.subCategoryTQHeading);
+            subCategory.setText(userInput.getSubCategory().get(0).getSubCategory());
+        }
+
+        private void updateTotalQuestionAnswered() {
             CustomTextView questionNum = (CustomTextView) findViewById(R.id.questionNum);
-            questionNum.setText(String.valueOf(questionIndex+1) + " of "+String.valueOf(questionList.size()));
+            questionNum.setText(String.valueOf(questionIndex + 1) + " of " + String.valueOf(questionList.size()));
+        }
+
+        private void updateTotalCorrect() {
             CustomTextView score = (CustomTextView) findViewById(R.id.score);
             score.setText(String.valueOf(totalCorrect));
-
-
         }
 
         /**
          * This method will called everytime a question has to be updated in the UI
-         * @param questionToDisplay
+         * @param displaySnackBar
+         * @param explantionToDisplay
          */
-        void updateQuestion(Question questionToDisplay) {
-            questionIndex++;
+        void updateQuestion(boolean displaySnackBar, String explantionToDisplay) {
+            if(questionIndex == questionList.size()) {
+                // If all the questions were already asked show results
+                Intent showResult = new Intent(this, QuizResults.class);
+                showResult.putExtras(getIntent().getExtras());
+                String quizResult = String.valueOf(totalCorrect) + ActivityConstants.OUTOF + String.valueOf(questionList.size());
+                showResult.putExtra(ActivityConstants.QUIZRESULTS, quizResult);
+              //  showResult.putExtra(ActivityConstants.QUIZAGAIN, offset);
+                startActivity(showResult);
+                return;
+            }
+            Question questionToDisplay = questionList.get(questionIndex);
+            CustomButton nextQuestion = (CustomButton) findViewById(R.id.nextbutton);
+            if(displaySnackBar) {
+                // This is not the first question display the result and explanation of previous question
+                if(questionToDisplay.getExplanation() != null)  {
+                    explantionToDisplay+=   questionToDisplay.getExplanation();
+                }
+                // When user clicks the Ok button on the Snackbar display the next question
+                View.OnClickListener mOnClickListener = new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                            updateQuestion(Boolean.FALSE, null);
+                    }
+                };
 
+
+                /* Disable the Next question button when SnackBar is displayed */
+                nextQuestion.setEnabled(Boolean.FALSE);
+                /*Display the snackbar*/
+                Snackbar snackBar = Snackbar.make(findViewById(android.R.id.content), explantionToDisplay, Snackbar.LENGTH_INDEFINITE).setAction("Ok", mOnClickListener);
+                //To increase the explanation space.
+                View snackbarView = snackBar.getView();
+                TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+                textView.setMaxLines(5);  // show multiple line
+                snackBar.show();
+
+                questionIndex++;
+                return;
+            }
+
+
+            nextQuestion.setEnabled(Boolean.TRUE);
+            currentCorrectAnswer = questionToDisplay.getCorrectAnswer();
             // Set the question text
             CustomTextView questionText = (CustomTextView)findViewById(R.id.questionText);
             String questionString = questionToDisplay.getQuestionText();
             questionText.setText(questionString);
-
+            RadioGroup cleanRadioGroup = (RadioGroup) findViewById(R.id.answerOption);
+            cleanRadioGroup.clearCheck();
             // Option one
 
-            CustomTextView optionOne = (CustomTextView)findViewById(R.id.optionOneText);
+            CustomRadioButton optionOne = (CustomRadioButton)findViewById(R.id.optionOneRadio);
             String optionOneText = questionToDisplay.getOptionOne();
             optionOne.setText(optionOneText);
+            optionOne.setFont(R.color.colorText);
 
             // Option two
 
-            CustomTextView optionTwo = (CustomTextView)findViewById(R.id.optionTwoText);
+            CustomRadioButton optionTwo = (CustomRadioButton)findViewById(R.id.optionTwoRadio);
             String optionTwoText = questionToDisplay.getOptionTwo();
             optionTwo.setText(optionTwoText);
+            optionTwo.setFont(R.color.colorText);
 
             // Option three
 
-            CustomTextView optionThree = (CustomTextView)findViewById(R.id.optionThreeText);
+            CustomRadioButton optionThree = (CustomRadioButton)findViewById(R.id.optionThreeRadio);
             String optionThreeText = questionToDisplay.getOptionThree();
             optionThree.setText(optionThreeText);
+            optionThree.setFont(R.color.colorText);
 
             // Option four
 
-            CustomTextView optionFour = (CustomTextView)findViewById(R.id.optionFourText);
+            CustomRadioButton optionFour = (CustomRadioButton)findViewById(R.id.optionFourRadio);
             String optionFourText = questionToDisplay.getOptionFour();
             optionFour.setText(optionFourText);
+            optionFour.setFont(R.color.colorText);
+            // Displaying the explanation
+
         }
 
         /**
@@ -99,26 +174,30 @@ import java.util.List;
          * @param inputView
          */
         public void nextQuestion(View inputView)  {
-            RadioGroup quizAnswer = (RadioGroup) findViewById(R.id.answerOption);
-            int userEnteredAnswer = quizAnswer.getCheckedRadioButtonId();
-            // TODO : If userEnteredAnswer == -1 that means the user has not chosen any option
-            // Should we display a toast asking him to choose a option???
 
-            if(userEnteredAnswer == -1) {
-                Toast.makeText(this,"No answer selected(?)",Toast.LENGTH_SHORT).show();
-            }
-            else {
+            CustomRadioButton correctAnswerRadio = (CustomRadioButton) findViewById(answerOptionRadioButton[(currentCorrectAnswer - 1)]);
                 // Get the radio button
-                RadioButton userAnswer = (RadioButton) findViewById(userEnteredAnswer);
-                if (currentCorrectAnswer.equals(userAnswer.getText().toString())) {
-                    Toast.makeText(this, "Right Answer!", Toast.LENGTH_SHORT).show();
-                    updateQuestion(questionList.get(questionIndex));
-                }
+            if (correctAnswerRadio.isChecked()) {
+                totalCorrect++;
+                updateTotalCorrect();
+                updateQuestion(Boolean.TRUE, ActivityConstants.RIGHT_ANSWER);
+                updateTotalQuestionAnswered();
             }
+            else    {
+                RadioGroup userEnteredAnswerGroup = (RadioGroup) findViewById(R.id.answerOption);
+                CustomRadioButton userEnteredAnswer = (CustomRadioButton) findViewById(userEnteredAnswerGroup.getCheckedRadioButtonId());
+
+                correctAnswerRadio.setFont(R.color.correctAnswer);
+                userEnteredAnswer.setFont(R.color.wrongAnswer);
+                updateQuestion(Boolean.TRUE, ActivityConstants.WRONG_ANSWER );
+                updateTotalQuestionAnswered();
+            }
+
 
         }
 
         public void changeCategory(View inputView) {
 
         }
+
     }
