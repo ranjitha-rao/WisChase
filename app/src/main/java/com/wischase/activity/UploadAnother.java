46f598f37b67e297e33e34c323723b20a4e3b74f
disha <gayathriraju84@gmail.com>
2016-04-23 11:40:55 -0500

    Adding UploadAnother file for uploading next question

---
+++ app/src/main/java/com/wischase/activity/UploadAnother.java
@@ -0,0 +1,46 @@
+package com.wischase.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.wischase.Category;
+import com.wischase.R;
+
+public class UploadAnother extends AppCompatActivity {
+
+    int grade;
+    Category userInput;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_another);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+Intent getIntent=getIntent();
+        userInput=(Category)(getIntent.getParcelableExtra(ActivityConstants.USER_INPUT));
+        grade=getIntent.getIntExtra(ActivityConstants.GRADE_INPUT,0);
+        TextView text;
+        Toast.makeText(this,"Successfully Uploaded",Toast.LENGTH_LONG).show();
+    }
+public void anotherQuestion(View view)
+{
+    Intent intent=new Intent(this, QuestionUpload.class);
+    intent.putExtra(ActivityConstants.USER_INPUT,userInput);
+    intent.putExtra(ActivityConstants.GRADE_INPUT,grade);
+    startActivity(intent);
+
+}
+    public void mainMenu(View view)
+    {
+        Intent intent=new Intent(this, Selectionscreen.class);
+        startActivity(intent);
+
+    }
+}
