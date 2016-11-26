e2208e5ce00e6872650bc12c44ac1461da602ea4
sri_suki <sukiwillbe1981@gmail.com>
2016-11-26 01:50:20 -0600

    Shared preference updates as of Nov 26

--- app/src/main/java/com/wischase/utils/SharedPref.java
+++ app/src/main/java/com/wischase/utils/SharedPref.java
@@ -13,9 +13,7 @@ import com.wischase.activity.ActivityConstants;
 public class SharedPref {
 
     //Shared Preference instance
-    public static final String MyPREFERENCES = "MyPrefs";
-    public static final String Uid = "UserId";
-    public static final String Uname = "UserName";
+
     SharedPreferences settings;
     Editor editor;
 
@@ -25,10 +23,10 @@ public class SharedPref {
 
    public void save(Context context, Long UserId, String UsrName) {
         //Put the values in the shared preference
-        settings = context.getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        settings = context.getSharedPreferences(ActivityConstants.MyPREFERENCES, Context.MODE_PRIVATE);
         editor = settings.edit();
         editor.putLong(ActivityConstants.USER_ID, UserId);
-        editor.putString(Uname, UsrName);
+        editor.putString(ActivityConstants.USER_NAME, UsrName);
         editor.commit();
        //Toast to test if this method has been invoked. Need to remove after testing
        Toast.makeText(context, "Save Method is called", Toast.LENGTH_LONG).show();
@@ -37,22 +35,22 @@ public class SharedPref {
     // Get the User Id saved as Shared Preference
     public Long getId(Context context) {
         Long text1;
-        settings = context.getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-        text1 = settings.getLong(Uid,0);
+        settings = context.getSharedPreferences(ActivityConstants.MyPREFERENCES, Context.MODE_PRIVATE);
+        text1 = settings.getLong(ActivityConstants.USER_ID,0);
         return text1;
         }
 
     // Get the User Name saved as Shared Preference
     public String getUsrname(Context context) {
         String text2;
-        settings = context.getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-        text2= settings.getString(Uname,null);
+        settings = context.getSharedPreferences(ActivityConstants.MyPREFERENCES, Context.MODE_PRIVATE);
+        text2= settings.getString(ActivityConstants.USER_NAME,null);
         return text2;
     }
 
     //Clear Shared Preference values
     public void clearSharedPreference(Context context) {
-        settings = context.getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        settings = context.getSharedPreferences(ActivityConstants.MyPREFERENCES, Context.MODE_PRIVATE);
         editor = settings.edit();
         editor.clear();
         editor.commit();
